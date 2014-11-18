6dcc982c515d4f54c256ce0078d747846dfb142d
植草 智輝 <tmkueks19@gmail.com>
2014-11-18 23:50:12 +0900

    Toppageコントローラーの作成

--- app/Controller/ToppagesController.php
+++ app/Controller/ToppagesController.php
@@ -1,7 +1,12 @@
 <?php
 class ToppagesController extends AppController {
 	public $helpers = array('Html', 'Form');
+	public $uses = array('Article');
+	
 	public function index(){
 		// $this->set('users', $this->User->find('all'));
+		$allArticles = $this->Article->find('all');
+		 
+		$this->set('articles',$this->Article->find('all'));
 	}
 }
